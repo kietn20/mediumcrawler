32bffbe0d365be350e7b349f6d17dbac193dc961
kietn20 <kietnguyen3698@gmail.com>
2025-01-20 00:15:37 -0800

    working

--- frontend/src/components/Collage.jsx
+++ frontend/src/components/Collage.jsx
@@ -186,7 +186,7 @@ export const Collage = () => {
           </div>
         ))}
       </div>
-      <div ref={dockRef} className="sticky bottom-2 left-1/2 max-w-full -translate-x-1/2 opacity-30 hover:opacity-100 duration-300 z-50">
+      <div ref={dockRef} className="sticky bottom-0 left-1/2 max-w-full -translate-x-1/2 opacity-30 hover:opacity-100 duration-300 z-50">
         <Dock className="items-end pb-1">
           {dockElements.map((item, idx) => (
             <DockItem
